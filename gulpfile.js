62e942a7eb45d1b1bc16289c0ffd72a6aba09a72
Aster Valdemoro <astervaldemoro@gmail.com>
2018-08-27 09:06:03 -0400

    implement sass and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 var gulp = require('gulp');
 var uglify = require('gulp-uglify');
+var sass = require('gulp-sass');
+var autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('default', function() {
     console.log('hello there!');
@@ -9,4 +11,14 @@ gulp.task('js', () => {
   return gulp.src(['js/dbhelper.js', 'js/main.js', 'js/restaurant_info.js'])
     .pipe(uglify())
     .pipe(gulp.dest('dist'));
+});
+
+gulp.task('styles', function (done) {
+    gulp.src('sass/**/*.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(autoprefixer({
+            browsers: ['last 2 versions']
+        }))
+        .pipe(gulp.dest('./css'));
+    done();
 });
